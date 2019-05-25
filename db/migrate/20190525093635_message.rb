ab37534433987d7588c8c73ae78104ab912ca545
emono <ababababa0222@gmail.com>
2019-05-25 19:24:37 +0900

    add good_count

--- db/migrate/20190525093635_message.rb
+++ db/migrate/20190525093635_message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Migration[5.2]
   def up
     add_column :messages, :to_user, :string
+    add_column :messages, :good, :integer, default: 0
     change_column :messages, :to_user, :string, :null => false
     change_column :messages, :context, :test, :null => false
   end
